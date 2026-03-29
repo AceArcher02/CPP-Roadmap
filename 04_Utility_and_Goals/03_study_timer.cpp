c80d2fa81f379ec059acd687951091caa9e34694
AceArcher02 <acearcher02@gmail.com>
2026-03-29 17:42:24 +0530

    Add countdown timer for study session launch
    Implement a countdown before starting a study session.

---
+++ 04_Utility_and_Goals/03_study_timer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 10; i > 0; i--) {
+        cout << "Launching Study Session in... " << i << endl;
+    }
+    cout << "FOCUS MODE ON." << endl;
+    return 0;
+}
