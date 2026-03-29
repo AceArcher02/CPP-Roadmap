f169594e56d7cb6afc13b28858f3d0acec897cb8
AceArcher02 <acearcher02@gmail.com>
2026-03-29 17:44:31 +0530

    Add cycle_check.cpp for remainder calculation

---
+++ 03_Mathematical_Models/03_cycle_check.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+    // Useful for finding cycles in algorithms
+    cout << "Remainder when divided by 7: " << num % 7 << endl;
+    return 0;
+}
