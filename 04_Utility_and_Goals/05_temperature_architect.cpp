40955304cf54fbb331cebe7864bc35fd95d65eb1
AceArcher02 <acearcher02@gmail.com>
2026-03-31 14:28:25 +0530

    Create 05_temperature_architect.cpp

---
+++ 04_Utility_and_Goals/05_temperature_architect.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;
+    cout << "Enter Celsius: ";
+    cin >> celsius;
+    fahrenheit = (celsius * 9 / 5) + 32;
+    cout << "Fahrenheit: " << fahrenheit << endl;
+    return 0;
+}
