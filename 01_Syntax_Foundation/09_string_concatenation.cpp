21ac9d1f7bf0c27f3a2f2dc3c5bcae6c609ab762
AceArcher02 <acearcher02@gmail.com>
2026-04-04 15:25:35 +0530

    Create 09_string_concatenation.cpp

---
+++ 01_Syntax_Foundation/09_string_concatenation.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string fName = "Lily", lName = "Waffles";
+    std::string full = fName + " " + lName;
+    std::cout << full << std::endl;
+    return 0;
+}
